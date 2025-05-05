dbcc39db716233f8dd288cff587344d3fbb73cc4
Virgil Shaun Koh <virgilskoh@gmail.com>
2025-05-05 16:21:43 +0200

    we test the changes tomorrow

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/login'
+    },
+    {
       path: '/login',
       name: 'login',
       component: LoginView,
